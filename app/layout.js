14cb4dfc20621eeb7cebd6108aa4b8bdd467ec8b
tbrown034 <tbrown034@gmail.com>
2025-03-11 17:20:46 -0400

    wip: trying suspens in layout

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import "./globals.css";
 import Header from "@/Components/Layout/Header/Header";
 import Footer from "@/Components/Layout/Footer/Footer";
@@ -14,7 +15,9 @@ export default function RootLayout({ children }) {
       <body className="flex p-2 flex-col min-h-screen bg-white text-black dark:bg-black dark:text-white transition-colors duration-300">
         <YearProvider>
           <Header />
-          <main className="flex-grow p-4 flex">{children}</main>
+          <main className="flex-grow p-4 flex">
+            <Suspense fallback={<div>Loading...</div>}>{children}</Suspense>
+          </main>
           <Footer />
         </YearProvider>
       </body>
