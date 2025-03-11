771fb531367102b24b4ad9fe8c4bc3b5c2c1a237
tbrown034 <tbrown034@gmail.com>
2025-03-11 19:51:58 -0400

    wip

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,18 @@ const nextConfig = {
   images: {
     domains: ["image.tmdb.org"],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/movies",
+        destination: "/Movies",
+      },
+      {
+        source: "/tv",
+        destination: "/Tv",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
