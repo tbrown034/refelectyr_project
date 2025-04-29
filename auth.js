dbccf1eb3780e1d15217f81dfb3396ff1f6af8f9
tbrown034 <tbrown034@gmail.com>
2025-04-29 17:22:11 -0400

    wip, auth

--- auth.js
+++ auth.js
@@ -1,6 +1,17 @@
+// auth.js needs this update
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      return true;
+    },
+  },
 });
