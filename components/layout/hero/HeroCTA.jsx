bb956d6df00e7d7c99d72dc57cb616fa9c338540
tbrown034 <tbrown034@gmail.com>
2025-04-29 18:25:06 -0400

    removed auth

--- components/layout/hero/HeroCTA.jsx
+++ components/layout/hero/HeroCTA.jsx
@@ -1,6 +1,5 @@
 import HeroAddButtons from "./HeroButtons/HeroAddButtons";
 import HeroLogButtons from "./HeroButtons/HeroLogButtons";
-import SignIn from "@/components/ui/auth/signIn";
 
 export default function HeroCTA() {
   return (
@@ -14,9 +13,6 @@ export default function HeroCTA() {
           Account Access
         </h2>
         <HeroLogButtons />
-        <div className="rounded-xl p-2 px-4 hover:bg-blue-700 active:scale-95 transition-all bg-blue-600 text-white font-medium cursor-pointer border-4 border-blue-600">
-          <SignIn />
-        </div>
       </div>
     </div>
   );
